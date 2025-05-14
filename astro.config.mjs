dd4caa2faee7ad311e000b0278b38c567cc94960
Rat0 <dorks@Rat0.com>
2025-05-14 01:14:03 -0400

    fix: change site URL from https to http in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Enable static site generation
   output: 'static',
-  site: 'http://travis.lol',
+  site: 'http://travis.lol', // Changed https back to http
   base: '/', // Correct base for custom domain
   trailingSlash: 'always',
 });
